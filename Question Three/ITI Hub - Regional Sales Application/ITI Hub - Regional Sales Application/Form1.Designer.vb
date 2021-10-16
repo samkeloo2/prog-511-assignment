e5e113728d8436f6917af52430d4e9cac2a58888
Shaiyur Dooken <91010705+shai-04@users.noreply.github.com>
2021-10-16 06:43:41 +0200

    Add files via upload

---
+++ Question Three/ITI Hub - Regional Sales Application/ITI Hub - Regional Sales Application/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picLogo = New System.Windows.Forms.PictureBox()
+        Me.rtbOutput = New System.Windows.Forms.RichTextBox()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnEnterSales = New System.Windows.Forms.Button()
+        Me.chkGenerateAuto = New System.Windows.Forms.CheckBox()
+        CType(Me.picLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picLogo
+        '
+        Me.picLogo.Image = Global.WindowsApplication1.My.Resources.Resources.itiLogo
+        Me.picLogo.Location = New System.Drawing.Point(13, 13)
+        Me.picLogo.Name = "picLogo"
+        Me.picLogo.Size = New System.Drawing.Size(205, 204)
+        Me.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picLogo.TabIndex = 0
+        Me.picLogo.TabStop = False
+        '
+        'rtbOutput
+        '
+        Me.rtbOutput.Location = New System.Drawing.Point(240, 12)
+        Me.rtbOutput.Name = "rtbOutput"
+        Me.rtbOutput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical
+        Me.rtbOutput.Size = New System.Drawing.Size(823, 371)
+        Me.rtbOutput.TabIndex = 1
+        Me.rtbOutput.Text = ""
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(12, 306)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(205, 31)
+        Me.btnDisplay.TabIndex = 2
+        Me.btnDisplay.Text = "&Display Sales"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(13, 353)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(205, 30)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnEnterSales
+        '
+        Me.btnEnterSales.Location = New System.Drawing.Point(13, 223)
+        Me.btnEnterSales.Name = "btnEnterSales"
+        Me.btnEnterSales.Size = New System.Drawing.Size(205, 31)
+        Me.btnEnterSales.TabIndex = 4
+        Me.btnEnterSales.Text = "&Enter Sales"
+        Me.btnEnterSales.UseVisualStyleBackColor = True
+        '
+        'chkGenerateAuto
+        '
+        Me.chkGenerateAuto.AutoSize = True
+        Me.chkGenerateAuto.Location = New System.Drawing.Point(13, 270)
+        Me.chkGenerateAuto.Name = "chkGenerateAuto"
+        Me.chkGenerateAuto.Size = New System.Drawing.Size(213, 21)
+        Me.chkGenerateAuto.TabIndex = 5
+        Me.chkGenerateAuto.Text = "Generate sales automatically"
+        Me.chkGenerateAuto.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1075, 395)
+        Me.Controls.Add(Me.chkGenerateAuto)
+        Me.Controls.Add(Me.btnEnterSales)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.rtbOutput)
+        Me.Controls.Add(Me.picLogo)
+        Me.Name = "Form1"
+        Me.Text = "ITI Hub : Regional Sales"
+        CType(Me.picLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picLogo As PictureBox
+    Friend WithEvents rtbOutput As RichTextBox
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnEnterSales As Button
+    Friend WithEvents chkGenerateAuto As CheckBox
+End Class
---
+++ Question Three/ITI Hub - Regional Sales Application/ITI Hub - Regional Sales Application/Form1.vb
@@ -0,0 +1,138 @@
+﻿' Shaiyur Dooken - 151 757
+' Programming 511 - Question Three
+' ITI Hub : Regional Sales
+Public Class Form1
+    'Global Variables:
+    Dim intRows As Integer = 5
+    Dim intColumns As Integer = 3
+    Dim nl As String = vbNewLine
+
+    ' Creates a global 6x3 2D Array to store the regional sales:
+    Dim intArraySales(intRows, intColumns) As Integer
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Closes the entire application
+        Me.Close()
+    End Sub
+
+    Private Sub btnEnterSales_Click(sender As Object, e As EventArgs) Handles btnEnterSales.Click
+        Dim strRegion As String = ""
+
+        ' Automatically generate contributions based on the assignment booklet.
+        If chkGenerateAuto.Checked Then
+            AutomaticArray()
+        Else
+            Try
+                ' Allows the user to enter the region's sales manually. If there's an error,
+                ' the application will automatically revert to the hardcoded array.
+                For intMonth As Integer = 0 To intRows
+                    For intRegion As Integer = 1 To intColumns
+                        If intRegion = 1 Then
+                            strRegion = "Kwa-Zulu-Natal"
+                        ElseIf intRegion = 2 Then
+                            strRegion = "Gauteng"
+                        Else
+                            strRegion = "Western Cape"
+                        End If
+
+                        intArraySales(intMonth, intRegion) = InputBox("Enter the sales for " & strRegion &
+                                                                      " , Month " & intMonth + 1 & ":",
+                                                                      "Notice:", "0")
+                    Next intRegion
+                Next intMonth
+
+            Catch ex As Exception
+                MsgBox("We've detected an exception being raised. Reverting to hard-coded array...")
+                AutomaticArray()
+            End Try
+
+        End If
+
+        rtbOutput.AppendText("The array containing the regional sales has been generated successfully..." & nl)
+
+        btnDisplay.Enabled = True
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        Dim intKZN As Integer = 0
+        Dim intGau As Integer = 0
+        Dim intWC As Integer = 0
+        Dim intSum As Integer = 0
+
+        rtbOutput.Clear()
+
+        rtbOutput.SelectionTabs = {50, 200, 300, 450}
+
+        rtbOutput.AppendText("Month:" & vbTab & "Kwa-Zulu-Natal Sales:" & vbTab & "Gauteng Sales:" &
+                             vbTab & "Western Cape Sales:" & nl)
+        rtbOutput.AppendText(nl)
+
+        'Use this for output:
+        For intMonth As Integer = 0 To intRows
+            rtbOutput.AppendText(intMonth + 1 & vbTab)
+
+            For intRegion As Integer = 1 To intColumns
+                rtbOutput.AppendText(intArraySales(intMonth, intRegion).ToString("C0") & vbTab)
+
+                If intRegion = 1 Then
+                    intKZN += intArraySales(intMonth, intRegion)
+                ElseIf intRegion = 2 Then
+                    intGau += intArraySales(intMonth, intRegion)
+                Else
+                    intWC += intArraySales(intMonth, intRegion)
+                End If
+            Next intRegion
+
+            rtbOutput.AppendText(nl)
+        Next intMonth
+
+        intSum = intKZN + intGau + intWC
+
+        rtbOutput.AppendText(nl & "The total amount that ITI Hub made in all 3 regions is: " &
+                             intSum.ToString("C0") & nl)
+
+        rtbOutput.AppendText(nl & "Kwa-Zulu-Natal has contributed " & Math.Round(((intKZN / intSum) * 100), 0) & "% " &
+                             " to the total." & nl)
+        rtbOutput.AppendText("Gauteng has contributed " & Math.Round((intGau / intSum) * 100, 0) & " %" &
+                             " to the total." & nl)
+        rtbOutput.AppendText("Western Cape has contributed " & Math.Round((intWC / intSum) * 100, 0) & " %" &
+                             " to the total." & nl)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Disables Display button until the array has been successfully generated
+        btnDisplay.Enabled = False
+
+        ' Automatically checks the automatic array generation for convenience
+        chkGenerateAuto.Checked = True
+
+        ' Disables maximize box in the title bar and prevents user from resizing the window border
+        Me.MaximizeBox = False
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+
+        ' Stretches the image to occupy the entire Picture Box
+        picLogo.SizeMode = PictureBoxSizeMode.StretchImage
+    End Sub
+
+    Private Sub AutomaticArray()
+        intArraySales(0, 1) = 12000
+        intArraySales(1, 1) = 190000
+        intArraySales(2, 1) = 175000
+        intArraySales(3, 1) = 188000
+        intArraySales(4, 1) = 125000
+        intArraySales(5, 1) = 163000
+
+        intArraySales(0, 2) = 90000
+        intArraySales(1, 2) = 85000
+        intArraySales(2, 2) = 80000
+        intArraySales(3, 2) = 83000
+        intArraySales(4, 2) = 87000
+        intArraySales(5, 2) = 80000
+
+        intArraySales(0, 3) = 65000
+        intArraySales(1, 3) = 64000
+        intArraySales(2, 3) = 71000
+        intArraySales(3, 3) = 67000
+        intArraySales(4, 3) = 65000
+        intArraySales(5, 3) = 64000
+    End Sub
+End Class
